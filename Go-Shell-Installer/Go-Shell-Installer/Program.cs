cd7edabbb737ffb729cdf85bea76187848aa69f7
Cameron Harp <charp5257@gmail.com>
2013-06-25 06:56:23 -0700

    Added the blank installer for Go-Shell.

---
+++ Go-Shell-Installer/Go-Shell-Installer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Go_Shell_Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powerShellPath = String.Format("{0}\\WindowsPowerShell", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            if(File.Exists(powerShellPath))
+            {
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
